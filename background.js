ef18bd8ef6dc3c254c821ec820438f0c0047237f
Bendik <knaben@OBOS.NO>
2019-11-20 14:21:31 +0100

    uppdate backbutton bug

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 function myfunc(){
 if(document.location["href"].endsWith("variables")){
 thisinterval = setInterval(function(){
+
     let dashboard=document.querySelector(".gtm-cloak-grey.gtm-cloaked-borderless.gtm-cloaked.gtm-cloaked-spinner.gtm-cloaked-ready");
-    if (dashboard){
-        let ettelement=document.querySelector(".gtm-cloak-grey.gtm-cloaked-borderless.gtm-cloaked.gtm-cloaked-spinner.gtm-cloaked-ready").firstElementChild;
-        let etannetelement = document.querySelector(".gtm-cloak-grey.gtm-cloaked-borderless.gtm-cloaked.gtm-cloaked-spinner.gtm-cloaked-ready").lastElementChild;
-        
+    if(dashboard.firstElementChild.innerText.split(" ")[0]==="User-Defined"){
+        clearInterval(thisinterval);
+    }else if (dashboard){
+        let ettelement=dashboard.firstElementChild;
+        let etannetelement = dashboard.lastElementChild;
         dashboard.insertBefore(etannetelement,ettelement);
         clearInterval(thisinterval);
         let usertable=document.querySelector("body > div.gtm-root > div > div.gtm-ng-view > gtm-variable-list > gtm-container-page > div > div > div.gtm-container-page-content-wrapper > div > div > div > div.card.card--table");
